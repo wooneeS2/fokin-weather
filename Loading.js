99009e5baa7618a74c6ab360d39ee7715e67a2dc
wooneeS2 <hello_world_@kakao.com>
2020-11-25 16:03:51 +0900

    111

---
+++ Loading.js
@@ -0,0 +1,27 @@
+import React from"react";
+import { StyleSheet, Text, View} from "react-native";
+
+export default function Loading(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}> 어썸하게 날씨 얻기 </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"flex-end",
+        paddingHorizontal: 30,
+        paddingVertical : 100,
+        backgroundColor: "#FDF6AA"
+    },
+
+    text: {
+        color:"#2c2c2c",
+        fontSize:30
+    }
+
+
+});
