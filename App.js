d160efb5ac528ebbccff2cf16a9604ef119ae5ed
wooneeS2 <hello_world_@kakao.com>
2020-11-28 00:50:26 +0900

    2-1

--- App.js
+++ App.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import {Alert} from 'react-native';
 import Loading from "./Loading";
+import * as Location from 'expo-location';
+import axios from "axios";
+import Weather from "./Weather";
 
 
-export default function App() {
-    return <Loading />;
+const API_KEY = "662a77cfba7d9096133ef335758f4464";
+
+export default class extends React.Component  {
+    state = {
+        isLoading : true
+    };
+    getWeather = async(latitude, longitude)=>{
+const { data} =await axios.get( 
+    `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+ );
+this.setState({isLoading: false, temp:data.main.temp});
+
+    };
+    getLocation= async()=> {
+        try{
+            await Location.requestPermissionsAsync();
+            const {
+                coords: { latitude, longitude }
+            } = await Location.getCurrentPositionAsync();
+            this.getWeather(latitude, longitude);
+            
+             
+        } catch (error){
+            Alert.alert("위치 찾기 불가");
+        }
+   
+        this.getLocation();
+        
+    }
+    render(){
+       const { isLoading, temp} = this.state;
+       return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+    }
     
   
 }
